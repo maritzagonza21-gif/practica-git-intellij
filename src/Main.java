14c83cdbc6545e11f286d8ef5c7b6e5f64375747
Maritza <maritzagonza21@gmail.com>
2026-05-12 05:22:00 +0200

    Añadidas tareas de ejemplo al inicio de la aplicación

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-
 public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
         System.out.println("=== GESTOR DE TAREAS ===");
+        gestor.agregarTarea(new Tarea("Preparar examen", false));
+        gestor.agregarTarea(new Tarea("Corregir prácticas", true));
+        gestor.agregarTarea(new Tarea("Subir materiales al aula virtual", false));
         gestor.listarTareas();
     }
 }
