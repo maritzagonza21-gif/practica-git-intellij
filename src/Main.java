c7206ab6c07b03073a24d19918b4906ce56e5d58
Maritza <maritzagonza21@gmail.com>
2026-05-16 19:04:41 +0200

    Resuelto conflicto entre menú y carga inicial de tareas

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
+        gestor.agregarTarea(new Tarea("Preparar examen", false));
+        gestor.agregarTarea(new Tarea("Corregir prácticas", true));
+        gestor.agregarTarea(new Tarea("Subir materiales al aula virtual", false));
         Scanner sc = new Scanner(System.in);
         int opcion;
         do {
