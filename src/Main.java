c98c1e7098feab1f62a2059f0dbda97e85c1fa2e
Maritza <maritzagonza21@gmail.com>
2026-05-16 19:19:13 +0200

    Integrada cabecera final en main

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int opcion;
         do {
-            System.out.println("=== GESTOR DE TAREAS ===");
+            System.out.println("=== ORGANIZADOR PERSONAL DE TAREAS ===");
             System.out.println("1. Listar tareas");
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
