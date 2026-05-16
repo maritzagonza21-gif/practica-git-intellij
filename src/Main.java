aa6e77a4a86edc546918828beec95aa251d3b674
Maritza <maritzagonza21@gmail.com>
2026-05-16 02:10:40 +0200

    Actualizada la cabecera principal de la aplicación.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         GestorTareas gestor = new GestorTareas();
-        System.out.println("=== GESTOR DE TAREAS ===");
+        System.out.println("=== ORGANIZADOR PERSONAL DE TAREAS ===");
         gestor.listarTareas();
     }
 }
